e9bf538312d8e739d2a5e2cc7f0190508a3e1121
wizzy <kluverdev@gmail.com>
2021-03-08 21:14:11 -0400

    Arguments

--- eats-backend/src/restaurant/restaurant.resolver.ts
+++ eats-backend/src/restaurant/restaurant.resolver.ts
@@ -1,11 +1,13 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 //CODING HERE AUTHOMATICALLY GENERATES A GRAPHQL SCHEMA.
 @Resolver((of) => Restaurant) //this is a resolver for the restaurant entity
 export class restaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
+
+ 
 }
