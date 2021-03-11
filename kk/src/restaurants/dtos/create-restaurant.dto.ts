355c50c4478c022ad09955ca8219b920e25ff4fb
wizzy <kluverdev@gmail.com>
2021-03-11 12:10:23 -0400

    mapped types

--- kk/src/restaurants/dtos/create-restaurant.dto.ts
+++ kk/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,26 +1,7 @@
-import { ArgsType, Field } from '@nestjs/graphql';
+import { ArgsType, Field, InputType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurant.entity';
 
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field(type => String)
-  @IsString()
-  @Length(5, 10)
-  name: string;
-
-  @Field(type => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
-
-  @Field(type => String)
-  @IsString()
-  address: string;
-
-  @Field(type => String)
-  @IsString()
-  ownersName: string;
-
-  @Field(type => String)
-  @IsString()
-  categoryName: string;
-}
+@InputType()
+//we are creating our dto from Restaurant and omiting the id.
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']){}
--- kk/src/restaurants/entities/restaurant.entity.ts
+++ kk/src/restaurants/entities/restaurant.entity.ts
@@ -1,7 +1,12 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 //we are using this file for both typeorm entities and graphql
+//this file is now generating out dto,graphql schema and db tables. creating objectypes with same name as
+//as input type is not good === errors. we make the input type abstract to remove the errors.
+
+@InputType({isAbstract: true}) //we dont want this to go to the schema
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -11,21 +16,27 @@ export class Restaurant {
 
   @Field(type => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(type => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field(type => String)
   @Column()
+  @IsString()
   ownersName: string;
 
   @Field(type => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
