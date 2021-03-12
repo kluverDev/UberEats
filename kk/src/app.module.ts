21b13468bdd239ca9efc2496d8fe53512dccd81b
wizzy <kluverdev@gmail.com>
2021-03-12 11:07:24 -0400

    user Model

--- kk/src/app.module.ts
+++ kk/src/app.module.ts
@@ -6,6 +6,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import * as joi from 'joi';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -34,12 +37,15 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
-      entities: [Restaurant]//RESTAURANT IS GOING TO THE DB.TYPORM IS AWARE OF THE ENTITY 
+      entities: [User]
+      //entities: [Restaurant]//RESTAURANT IS GOING TO THE DB.TYPORM IS AWARE OF THE ENTITY 
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    RestaurantsModule,
+   // RestaurantsModule,
+    UsersModule,
+   CommonModule,
   ],
   controllers: [],
   providers: [],
