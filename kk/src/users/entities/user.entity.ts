56ee50e0ff6491256369ca3502706106518af232
wizzy <kluverdev@gmail.com>
2021-03-13 10:29:35 -0400

    Hashing passwords

--- kk/src/users/entities/user.entity.ts
+++ kk/src/users/entities/user.entity.ts
@@ -1,14 +1,22 @@
-import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+
 
 enum UserRole {
   Client,
   Owner,
-  Delivery
+  Delivery,
 }
 
-registerEnumType(UserRole, {name: 'UserRole'})
+registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -22,7 +30,17 @@ export class User extends CoreEntity {
   @Field(type => String)
   password: string;
 
-  @Column({type: 'enum', enum: UserRole})
+  @Column({ type: 'enum', enum: UserRole })
   @Field(type => UserRole)
   role: UserRole;
+
+  @BeforeInsert()//encrypting pssword before it is saved on the db
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException()
+    }
+  }
 }
