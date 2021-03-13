79bdf60b4f34756ce78a5588a51eff499bdf6818
wizzy <kluverdev@gmail.com>
2021-03-12 21:11:24 -0400

    create account mutation part 2

--- kk/src/users/users.service.ts
+++ kk/src/users/users.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "./entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User) private readonly users: Repository<User>,
-    ){}
+  constructor(
+    @InjectRepository(User) private readonly users: Repository<User>,
+  ) {}
+  async createAccount({ email, password, role }: CreateAccountInput) {
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        //make error message
+        return;
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (error) {
+      //make error message
+      return;
+    }
+  }
 }
